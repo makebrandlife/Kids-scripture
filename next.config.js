c7064e1490e34b3b9dbac80e771373e717738268
makebrandlife <learnconow@gmail.com>
2025-10-24 12:59:28 +0530

    Add Next.js configuration for images and compression

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['i.ytimg.com', 'img.youtube.com'],
+  },
+  compress: true,
+}
+
+module.exports = nextConfig
